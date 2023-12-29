4447f354bee19a0b33adaa215a208cd81e39fe8d
Saad Hesham <132556058+Saad-Hisham@users.noreply.github.com>
2023-12-29 02:55:33 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './app.css'
+import './App.css'
 import { Provider } from 'react-redux'
 import store from './assets/components/Redux/Store.js'
 
